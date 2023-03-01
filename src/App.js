d2d4e274fdf5151e07bce7d363324c6627f16a7c
NikRych223 <KyrychenkoN023@gmail.com>
2023-03-01 11:05:44 +0200

    add error msg for input

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const darkTheme = createTheme({
     palette: {
       mode: 'dark',
     },
-  });
+});
 
 const App = () => {
 
--- src/components/add-new-task/AddNewTask.js
+++ src/components/add-new-task/AddNewTask.js
@@ -7,28 +7,56 @@ const AddNewTask = (props) => {
     const {setTodoArray, newID} = props;
 
     const [inputText, setInputText] = useState("");
+    const [error, setError] = useState(false);
 
     const addNewItem = () => {
+        if (!error) {
+            if (inputText === "" || inputText === " ") {
+                setError(true);
+                return;
+            }
+    
+            const newTask = {
+                id: newID(),
+                text: inputText,
+                completed: false
+            };
+    
+            setTodoArray(prev => {
+                return [...prev, newTask];
+            });
+    
+            setInputText("");
+        }
+    };
 
-        const newTask = {
-            id: newID(),
-            text: inputText,
-            completed: false
-        };
+    const onHandleChange = (e) => {
+        const value = e.target.value;
+        setInputText(value);
 
-        setTodoArray(prev => {
-            return [...prev, newTask];
-        });
+        const checkNotEmpy = value === null || value === "" || value === " ";
+        const checkLength = value.length < 5 || value.length > 20;
 
-        setInputText("");
-    }
+        if (checkNotEmpy || checkLength) {
+            setError(true);
+        } else {
+            setError(false);
+        }
+    };
 
     return (
         <Container>
             <div className="add-new-task">
                 <Box sx={{mt: 2, display: "flex", justifyContent: "center", alignItems: "baseline", gap: "2rem"}}>
                     <Button variant="contained" onClick={addNewItem}>Add</Button>
-                    <TextField id="outlined-basic" label="Input your task" variant="outlined" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+                    <TextField 
+                        id="outlined-basic" 
+                        label={error ? "Error" : "Input your task"} 
+                        variant="outlined" 
+                        value={inputText} 
+                        onChange={(e) => onHandleChange(e)}
+                        error={error ? true : false}
+                        helperText={error ? "Filed is empty" : null}/>
                 </Box>
             </div>
         </Container>
